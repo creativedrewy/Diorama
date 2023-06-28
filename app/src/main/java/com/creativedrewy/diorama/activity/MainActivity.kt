c71e6d1595574a748a86a149b5cb33d1a0c9697b
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-06-28 11:19:11 -0700

    Get a very basic 2d setup

--- app/src/main/java/com/creativedrewy/diorama/activity/MainActivity.kt
+++ app/src/main/java/com/creativedrewy/diorama/activity/MainActivity.kt
@@ -3,11 +3,13 @@ package com.creativedrewy.diorama.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -24,18 +26,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AndroidView(
-                        modifier = Modifier
-                            .width(300.dp)
-                            .height(300.dp),
-                        factory = { context ->
-                            val textureView = DioramaTextureView(context)
-                            textureView.setFrameRate(60.0)
-                            textureView.renderMode = ISurface.RENDERMODE_CONTINUOUSLY
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
+                        AndroidView(
+                            modifier = Modifier
+                                .width(300.dp)
+                                .height(500.dp),
+                            factory = { context ->
+                                val textureView = DioramaTextureView(context)
+                                textureView.setFrameRate(60.0)
+                                textureView.renderMode = ISurface.RENDERMODE_CONTINUOUSLY
 
-                            textureView
-                        }
-                    )
+                                textureView
+                            }
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/creativedrewy/diorama/rendering/DioramaTextureView.kt
+++ app/src/main/java/com/creativedrewy/diorama/rendering/DioramaTextureView.kt
@@ -5,12 +5,13 @@ import android.graphics.Color
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
+import org.rajawali3d.cameras.Camera2D
+import org.rajawali3d.cameras.OrthographicCamera
 import org.rajawali3d.materials.Material
-import org.rajawali3d.materials.textures.ATexture.TextureException
-import org.rajawali3d.materials.textures.Texture
-import org.rajawali3d.primitives.Sphere
+import org.rajawali3d.primitives.Plane
 import org.rajawali3d.renderer.Renderer
 import org.rajawali3d.view.TextureView
+import javax.microedition.khronos.opengles.GL10
 
 class DioramaTextureView @JvmOverloads constructor(
     context: Context,
@@ -30,23 +31,27 @@ class MyRenderer(
 ) : Renderer(context) {
 
     override fun initScene() {
-//        try {
         val material = Material()
         material.colorInfluence = 1f
         material.color = Color.RED
 
-        val sphere = Sphere(1f, 24, 24)
-        sphere.material = material
+        val plane = Plane(300f, 300f, 1, 1)
+        plane.material = material
 
-        currentScene.addChild(sphere)
-//        } catch (e: TextureException) {
-//            e.printStackTrace()
-//        }
+        currentScene.addChild(plane)
+    }
 
-        currentCamera.enableLookAt()
-        currentCamera.setLookAt(0.0, 0.0, 0.0)
-        currentCamera.setZ(6.0)
-        currentCamera.orientation = currentCamera.orientation.inverse()
+    override fun onRenderSurfaceSizeChanged(gl: GL10?, width: Int, height: Int) {
+        super.onRenderSurfaceSizeChanged(gl, width, height)
+
+        Log.v("Andrew", "Your width: $width, height: $height")
+
+        val orthoCamera = Camera2D()
+        orthoCamera.width = width.toDouble()
+        orthoCamera.height = height.toDouble()
+        orthoCamera.setProjectionMatrix(0, 0)
+
+        currentScene.switchCamera(orthoCamera)
     }
 
     override fun onTouchEvent(event: MotionEvent?) {

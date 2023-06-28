187e15dea0636d7cd9e7b80131e5d5bb12749d63
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-06-28 14:42:55 -0700

    Get rotate and scale gestures working on static object.

--- app/src/main/java/com/creativedrewy/diorama/activity/MainActivity.kt
+++ app/src/main/java/com/creativedrewy/diorama/activity/MainActivity.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.creativedrewy.diorama.rendering.DioramaTextureView
@@ -18,6 +22,8 @@ import com.creativedrewy.diorama.theme.DioramaTheme
 import org.rajawali3d.view.ISurface
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,12 +35,18 @@ class MainActivity : ComponentActivity() {
                     Box(
                         contentAlignment = Alignment.Center
                     ) {
+                        val ctx = LocalContext.current
+                        val textureView = remember { DioramaTextureView(ctx) }
+
                         AndroidView(
                             modifier = Modifier
                                 .width(300.dp)
-                                .height(500.dp),
-                            factory = { context ->
-                                val textureView = DioramaTextureView(context)
+                                .height(500.dp)
+                                .pointerInteropFilter { event ->
+                                    textureView.onTouchEvent(event)
+                                    true
+                                },
+                            factory = { _ ->
                                 textureView.setFrameRate(60.0)
                                 textureView.renderMode = ISurface.RENDERMODE_CONTINUOUSLY
 
--- app/src/main/java/com/creativedrewy/diorama/rendering/DioramaTextureView.kt
+++ app/src/main/java/com/creativedrewy/diorama/rendering/DioramaTextureView.kt
@@ -6,22 +6,31 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import org.rajawali3d.cameras.Camera2D
-import org.rajawali3d.cameras.OrthographicCamera
 import org.rajawali3d.materials.Material
+import org.rajawali3d.math.vector.Vector3
 import org.rajawali3d.primitives.Plane
 import org.rajawali3d.renderer.Renderer
 import org.rajawali3d.view.TextureView
 import javax.microedition.khronos.opengles.GL10
+import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.sqrt
 
 class DioramaTextureView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
 ) : TextureView(context, attrs) {
 
-    init {
-        val renderer = MyRenderer(context)
+    val renderer = MyRenderer(context)
 
+    init {
         setSurfaceRenderer(renderer)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        renderer.onTouchEvent(event)
+
+        return super.onTouchEvent(event)
     }
 
 }
@@ -30,12 +39,13 @@ class MyRenderer(
     context: Context
 ) : Renderer(context) {
 
+    val plane = Plane(300f, 300f, 1, 1)
+
     override fun initScene() {
         val material = Material()
         material.colorInfluence = 1f
         material.color = Color.RED
 
-        val plane = Plane(300f, 300f, 1, 1)
         plane.material = material
 
         currentScene.addChild(plane)
@@ -54,8 +64,91 @@ class MyRenderer(
         currentScene.switchCamera(orthoCamera)
     }
 
-    override fun onTouchEvent(event: MotionEvent?) {
+    private var previousX = 0f
+    private var previousY = 0f
+    private var previousRot = 0.0
 
+    private var fX = 0f
+    private var fY: Float = 0f
+    private var sX: Float = 0f
+    private var sY: Float = 0f
+    private var angle = 0f
+    private var lastAngle = 0f
+    private var startScale = 0.0
+    private var startDist = 0.0
+
+    override fun onTouchEvent(event: MotionEvent) {
+        val x: Float = event.x
+        val y: Float = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                sX = event.getX(0);
+                sY = event.getY(0);
+            }
+            MotionEvent.ACTION_POINTER_2_DOWN -> {
+                fX = event.getX(1);
+                fY = event.getY(1);
+
+                val xDist = abs(sX - fX)
+                val yDist = abs(sY - fY)
+                startDist = Math.sqrt((xDist * xDist + yDist * yDist).toDouble())
+                startScale = plane.scaleX
+
+                lastAngle = plane.rotX.toFloat()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (event.pointerCount == 1) {
+                    val dx: Float = x - previousX
+                    val dy: Float = y - previousY
+
+//                    plane.x += dx
+//                    plane.y += -dy
+                } else if (event.pointerCount == 2) {
+                    val nsX: Float = event.getX(0)
+                    val nsY: Float = event.getY(0)
+                    val nfX: Float = event.getX(1)
+                    val nfY: Float = event.getY(1)
+
+                    angle = angleBetweenLines(fX, fY, sX, sY, nfX, nfY, nsX, nsY)
+
+                    val xDist = abs(nsX - nfX)
+                    val yDist = abs(nsY - nfY)
+                    val newDist = sqrt((xDist * xDist + yDist * yDist).toDouble())
+
+                    plane.rotate(Vector3.Axis.Z, -(angle - lastAngle).toDouble())
+
+                    plane.setScale(startScale * (newDist / startDist))
+
+                    lastAngle = angle
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+//                ptrID1 = INVALID_POINTER_ID
+                lastAngle = 0f
+            }
+            MotionEvent.ACTION_POINTER_2_UP -> {
+//                ptrID2 = INVALID_POINTER_ID
+            }
+            MotionEvent.ACTION_CANCEL -> {
+//                ptrID1 = INVALID_POINTER_ID
+//                ptrID2 = INVALID_POINTER_ID
+            }
+        }
+
+        previousX = x
+        previousY = y
+    }
+
+    private fun angleBetweenLines(fX: Float, fY: Float, sX: Float, sY: Float, nfX: Float, nfY: Float, nsX: Float, nsY: Float): Float {
+        val angle1 = atan2((fY - sY).toDouble(), (fX - sX).toDouble()).toFloat()
+        val angle2 = atan2((nfY - nsY).toDouble(), (nfX - nsX).toDouble()).toFloat()
+        var angle = Math.toDegrees((angle1 - angle2).toDouble()).toFloat() % 360
+
+        if (angle < -180f) angle += 360.0f
+        if (angle > 180f) angle -= 360.0f
+
+        return angle
     }
 
     override fun onOffsetsChanged(xOffset: Float, yOffset: Float, xOffsetStep: Float, yOffsetStep: Float, xPixelOffset: Int, yPixelOffset: Int) { }

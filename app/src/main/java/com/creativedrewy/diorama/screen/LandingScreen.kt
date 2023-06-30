89f5252db75bd3ccef4b5303263a1890387a461c
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-06-30 14:31:00 -0700

    Get basic chooser grid in place

--- app/src/main/java/com/creativedrewy/diorama/screen/LandingScreen.kt
+++ app/src/main/java/com/creativedrewy/diorama/screen/LandingScreen.kt
@@ -1,20 +1,32 @@
 package com.creativedrewy.diorama.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -25,7 +37,7 @@ import org.rajawali3d.view.ISurface
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LandingScreen(
-    viewmodel: ConfiguratorViewModel = hiltViewModel()
+    configViewModel: ConfiguratorViewModel = hiltViewModel()
 ) {
     Box{
         val ctx = LocalContext.current
@@ -54,7 +66,42 @@ fun LandingScreen(
                 .height(300.dp)
                 .align(Alignment.BottomCenter),
         ) {
+            val uiState = configViewModel.configViewState.collectAsState().value
 
+            LazyVerticalGrid(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                columns = GridCells.Fixed(3),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                itemsIndexed(items = uiState.nftPreviews) { _, nft ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(1.0f)
+                            .background(Color.Red)
+                    )
+
+//                    GlideImage(
+//                        model = media.path,
+//                        contentDescription = stringResource(R.string.image_content_desc),
+//                        modifier = Modifier
+//                            .width(76.dp)
+//                            .aspectRatio(1.0f)
+//                            .clip(RoundedCornerShape(8.dp))
+//                            .background(color = MaterialTheme.colorScheme.surface)
+//                            .clickable {
+//                                navigateToDetails(media.path)
+//                            },
+//                        contentScale = ContentScale.Crop
+//                    ) {
+//                        it.thumbnail()
+//                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/creativedrewy/diorama/viewmodel/ConfiguratorViewModel.kt
+++ app/src/main/java/com/creativedrewy/diorama/viewmodel/ConfiguratorViewModel.kt
@@ -2,8 +2,38 @@ package com.creativedrewy.diorama.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
+
+data class NftPreview(
+    val id: String,
+    val uri: String,
+    val type: String = ""
+)
+
+data class ViewState(
+    val nftPreviews: List<NftPreview> = listOf()
+)
 
 @HiltViewModel
 class ConfiguratorViewModel @Inject constructor(): ViewModel() {
+
+    private val _configViewState = MutableStateFlow(ViewState())
+
+    val configViewState = _configViewState.asStateFlow()
+
+    init {
+        _configViewState.update {
+            ViewState(
+                (0..10).map {
+                    NftPreview(
+                        id = "1",
+                        uri = "https://cdn.pixabay.com/animation/2023/06/21/12/57/12-57-08-601_512.gif"
+                    )
+                }
+            )
+        }
+    }
 }
